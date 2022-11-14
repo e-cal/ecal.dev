2326fd013a15c6d8a852752b48de67917250f55f
ecal <ethan@ecal.dev>
2022-11-14 13:19:58 -0500

    Remove laggy cursor

--- src/app/App.js
+++ src/app/App.js
@@ -7,7 +7,6 @@ import {
 } from "react-router-dom";
 import AppRoutes from "./routes";
 import Headermain from "../header";
-import AnimatedCursor from "react-animated-cursor";
 import "./App.css";
 import { ParticleEffect } from "../components/particles.js";
 
@@ -24,17 +23,6 @@ export default function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <ParticleEffect />
-      <div className="cursor__dot">
-        <AnimatedCursor
-          innerSize={15}
-          outerSize={15}
-          color="255, 255 ,255"
-          outerAlpha={0.4}
-          innerScale={0.7}
-          outerScale={5}
-          trailingSpeed={1}
-        />
-      </div>
       <ScrollToTop>
         <Headermain />
         <AppRoutes />
